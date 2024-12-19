75c16358e593146c11d4f803d255391eec8a3108
dingj3Jiangding <dingj3@illinois.edu>
2024-12-19 00:31:34 -0600

    fix(jiefudi): delete button Issue#1

--- src/utils/setRole.js
+++ src/utils/setRole.js
@@ -4,6 +4,12 @@ export function setRole(role) {
 }
 
 export function getRole() {
+    // 判断运行环境是否为浏览器
+    if (typeof document === 'undefined') {
+      return 'visitor'; // 默认角色，服务端返回安全值
+    }
+  
+    // 浏览器中读取 Cookie
     const match = document.cookie.match(/(?:^|; )role=([^;]*)/);
     return match ? match[1] : 'visitor';
-}
+  }
